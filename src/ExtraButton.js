dba06d8c6466b983d48c6eaed8b63bf42cf7424b
ctxhou <ctxhou@gmail.com>
2018-07-01 18:51:51 +0800

    add disabled props to ExtraButton

--- src/ExtraButton.js
+++ src/ExtraButton.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const Wrapper = styled.button`
   float: right;
   border: 1px solid #eee;
   border-radius: 2px;
@@ -12,24 +12,40 @@ const Wrapper = styled.div`
   display: inline-block;
   color: #777;
   vertical-align: middle;
+  /* ${props => props.disabled ? `
+    pointer-events: none;
+    color: #AAA;
+    background: #F5F5F5;
+  ` 
+  : null} */
   &:hover {
     color: black;
     cursor: pointer;
+  }
+  &:disabled,
+  &[disabled]{
+    border: 1px solid grey;
+    background-color: #e7e7e7;
+    cursor: not-allowed;
   }
 `;
 
 type Props = {
   onClick: (event: any) => void,
+  disabled: boolean,
   children: React.Node
 };
 
 export default class ExtraButton extends React.PureComponent<Props> {
+  static defaultProps = {
+    disabled: false
+  }
+
   render() {
+    const {disabled, onClick} = this.props;
     return (
-      <Wrapper onClick={this.props.onClick}>
-        <div>
-          {this.props.children}          
-        </div>
+      <Wrapper onClick={onClick} disabled={disabled}>
+        {this.props.children}          
       </Wrapper>
     );
   }
